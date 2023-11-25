917af475f49c95971e261a2a07e9eeda17f2545f
Just2Deep <deepakrajsrs@gmail.com>
2023-11-25 18:50:51 +0530

    updated icon color

--- components/Features.tsx
+++ components/Features.tsx
@@ -53,7 +53,7 @@ interface FeatureProps {
 const FeatureItem = ({ title, description, icon, variant }: FeatureProps) => {
     return (
         <li className="flex w-full flex-1 flex-col items-start">
-            <div className={`rounded-full p-4 lg:p-7 bg-${variant}-50`}>
+            <div className={`rounded-full p-4 lg:p-7 bg-green-50`}>
                 <Image src={icon} alt="map" width={28} height={28} />
             </div>
             <h2 className="bold-20 lg:bold-32 mt-5 capitalize">{title}</h2>
